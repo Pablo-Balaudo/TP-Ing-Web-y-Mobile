d9fbf0f76ab7bc5a498731c71fabdc8ac8adf641
Pablo-Balaudo <Balaudo.Pablo@protonmail.com>
2020-08-22 00:09:51 -0300

    Test lógica HTML, y herencia de HTML
    Test de lógica HTML en el Foro
    
    Creación de Bases HTML
    
    Configuración de los HTML de apps/paginas para heredar de las Bases HTML

--- MyLittlePixel/Foro/views.py
+++ MyLittlePixel/Foro/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 
+Posts_Test = [{'Autor': 'Twilight Sparkle',
+               'Titulo': 'Intereses 1',
+               'Contenido': 'Me gustan los libros',
+               'Fecha_Publicacion': '21/08/2020'
+               },
+              {'Autor': 'Discord',
+               'Titulo': 'Intereses 2',
+               'Contenido': 'Me gusta trollear',
+               'Fecha_Publicacion': '22/08/2020'}]
+
 
 def foro(request):
-    return render(request, 'Foro/Foro.html')
+    contenido = {'Posts': Posts_Test}
+    return render(request, 'Foro/Foro.html', contenido)
 

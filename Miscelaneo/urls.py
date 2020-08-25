c75fc277b260ec527cc787a881f7c3c533ade94c
Lucas_Belmonte_Amenta <lucasbelmonteamenta2.0@gmail.com>
2020-08-25 02:02:18 -0300

    creacion a medias del registro de usuarios
    ademas de crear la aplicacion accesibilidad, realice el registro de usuarios, pero aun le faltan algunos retoques. Accedes a este por medio de la ruta de miscelaneo/Registrarse/

--- Miscelaneo/urls.py
+++ Miscelaneo/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.miscelaneo, name='Miscelaneo'),
+    path('Registrarse/', views.miscelaneo, name='Miscelaneo'),
 ]
--- Miscelaneo/views.py
+++ Miscelaneo/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login as do_login
+
+
+
+
 
 
 def miscelaneo(request):
-    return render(request, 'Miscelaneo/Miscelaneo.html')
+    formulario = UserCreationForm()
+    if request.method == "POST":
+        # el usuario está mandando sus datos
+        formulario = UserCreationForm(data=request.POST)
+        if formulario.is_valid():
+            usuario = formulario.save()
+            # Si el usuario se crea correctamente 
+            if usuario is not None:
+                do_login(request, usuario)
+                return redirect('')
+
+    # eliminamos el exceso de texto ayuda que se genera automaticamente             
+    formulario.fields['username'].help_text = None
+    formulario.fields['password1'].help_text = None
+    formulario.fields['password2'].help_text = None
+
+    return render(request, "Miscelaneo/registrarse.html", {'formulario': formulario})
 

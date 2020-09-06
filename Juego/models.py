7623345f564662c2b3450da02bbdc1e9fd75277c
Lucas_Belmonte_Amenta <lucasbelmonteamenta2.0@gmail.com>
2020-09-06 18:13:16 -0300

    Adapte la creacion de pixeles y lienzo para que se creen juntos

--- Juego/models.py
+++ Juego/models.py
@@ -1,19 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.postgres.fields import ArrayField
 from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
-
-class Pixel(models.Model):
-    color = models.PositiveIntegerField(default=0, validators=[MaxValueValidator(255)])
-    vidas = models.PositiveIntegerField(default=1, validators=[MaxValueValidator(5)])
-    coordenadaX = models.PositiveIntegerField(default=0, validators=[MaxValueValidator(50)])
-    coordenadaY = models.PositiveIntegerField(default=0, validators=[MaxValueValidator(50)])
-    #clave foranea
-    dueño = models.ForeignKey('auth.User', models.SET_NULL, blank=True, null=True,)
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
-
+TamañoLienzo = 51
 
 class Lienzo(models.Model):
     id = models.AutoField(primary_key=True)
@@ -21,3 +16,30 @@ class Lienzo(models.Model):
    # fechafin = models.DateTimeField()
     guardado = models.BooleanField(default=False)
     bloqueado = models.BooleanField(default=False)
+
+
+class Pixel(models.Model):
+    color = ArrayField(models.PositiveIntegerField(default=0, validators=[MaxValueValidator(255)]), size = 4)
+    vidas = models.PositiveIntegerField(default=1, validators=[MaxValueValidator(5)])
+
+    lienzo = models.ForeignKey(Lienzo, on_delete = models.CASCADE, null=True)
+    coordenadaX = models.PositiveIntegerField(default=0, validators=[MaxValueValidator(TamañoLienzo)])
+    coordenadaY = models.PositiveIntegerField(default=0, validators=[MaxValueValidator(TamañoLienzo)])
+
+    clave_primaria = [coordenadaX, coordenadaY, lienzo]
+
+    dueño = models.ForeignKey('auth.User', models.SET_NULL, blank=True, null=True)
+
+
+
+#Para que se creen los pixeles automaticamente tras crear un lienzo
+@receiver(post_save, sender=Lienzo)
+def CrearPixeles(sender, instance, created, **kwargs):
+    if created:
+        for X in range(TamañoLienzo):
+            for Y in range(TamañoLienzo):
+                Pixel.objects.create(coordenadaX=X, coordenadaY=Y ,lienzo=instance)        
+        
+@receiver(post_save, sender=Lienzo)
+def GuardarPixeles(sender, instance, **kwargs):
+    instance.pixel.save()    
--- Juego/views.py
+++ Juego/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Lienzo, Pixel
 
 
 def juego(request):
+
     return render(request, 'Juego/Juego.html')

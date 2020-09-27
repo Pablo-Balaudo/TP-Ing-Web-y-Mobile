c34fab3e7cab04236352ca42de4d77bfbd53a1e5
Pablo-Balaudo <Balaudo.Pablo@protonmail.com>
2020-09-27 00:02:11 -0300

    Admins can now make posts is Foro, re added tittle to pages.

--- Foro/models.py
+++ Foro/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
-class Base(models.Model):
-    idComentario = models.AutoField(primary_key=True)
-    fechainicio = models.DateTimeField(auto_now_add = True)
+
+class Post(models.Model):
+    parent_coment = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
--- Foro/views.py
+++ Foro/views.py
@@ -1,16 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Post
 
-Posts_Test = [{'Autor': 'Twilight Sparkle',
-               'Titulo': 'Intereses 1',
-               'Contenido': 'Me gustan los libros',
-               'Fecha_Publicacion': '21/08/2020'
-               },
-              {'Autor': 'Discord',
-               'Titulo': 'Intereses 2',
-               'Contenido': 'Me gusta trollear',
-               'Fecha_Publicacion': '22/08/2020'}]
+
+# class PostListView(ListView):
 
 
 def foro(request):
-    contenido = {'Posts': Posts_Test}
+    contenido = {'Posts': Post.objects.all()}
     return render(request, 'Foro/Foro.html', contenido)

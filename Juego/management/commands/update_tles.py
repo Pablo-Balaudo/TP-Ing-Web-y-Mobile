8c75bbf6f615926aec29ba61e73b2029e988f4ec
Lucas_Belmonte_Amenta <lucasbelmonteamenta2.0@gmail.com>
2020-09-30 22:19:09 -0300

    una pequeña cosita que se me olvidó del ultimo commit que hice

--- Juego/management/commands/update_tles.py
+++ Juego/management/commands/update_tles.py
@@ -1,24 +1,25 @@
 from django.core.management.base import BaseCommand
 
-from Juego.models import Color
-from django.core.serializers import deserialize
-import json
+
+def cargar_colores():
+
+    from Juego.models import Color
+    from django.core.serializers import deserialize
+    import json
+
+    ubicacion_archivo_json = 'Juego/fixtures/Colores.json'
+    with open(ubicacion_archivo_json) as json_datos:
+
+        colores = json.load(json_datos)
+
+        for key, value in colores.items():
+            Color.objects.update_or_create(Nombre=key, Red=value[0], Green=value[1], Blue=value[2], Alpha=value[3], defaults={'Nombre': key})
+
+        json_datos.close()
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
+        cargar_colores()
 
-        if Color.objects.all().count() < 149:
-            ubicacion_archivo_json = 'Juego/fixtures/Colores.json'
-            with open(ubicacion_archivo_json) as json_datos:
-
-                colores = json.load(json_datos)
-
-                for key, value in colores.items():
-                    color = Color.objects.create(Nombre=key, Red=value[0], Green=value[1], Blue=value[2],
-                                                 Alpha=value[3])
-                    if not Color.objects.filter(Nombre=key).exists():
-                        color.save()
-
-            json_datos.close()

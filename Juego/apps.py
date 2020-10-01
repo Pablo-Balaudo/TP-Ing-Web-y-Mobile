d273842905c5ff1acddd09bfe25bd8c8ded49620
Lucas_Belmonte_Amenta <lucasbelmonteamenta2.0@gmail.com>
2020-09-30 21:57:33 -0300

    Arregle el error de la carga de colores cuando estan repetidos y actualice los colores del HTML y del Json

--- Juego/apps.py
+++ Juego/apps.py
@@ -10,23 +10,15 @@ def cargar_colores():
     from django.core.serializers import deserialize
     import json
 
-    if Color.objects.all().count() < 149:
-        ubicacion_archivo_json = 'Juego/fixtures/Colores.json'
-        with open(ubicacion_archivo_json) as json_datos:
+    ubicacion_archivo_json = 'Juego/fixtures/Colores.json'
+    with open(ubicacion_archivo_json) as json_datos:
 
-            colores = json.load(json_datos)
+        colores = json.load(json_datos)
 
-            for key, value in colores.items():
-                color = Color.objects.create(
-                    Nombre=key,
-                    Red=value[0],
-                    Green=value[1],
-                    Blue=value[2],
-                    Alpha=value[3])
-                if not Color.objects.filter(Nombre=key).exists():
-                    color.save()
+        for key, value in colores.items():
+            Color.objects.update_or_create(Nombre=key, Red=value[0], Green=value[1], Blue=value[2], Alpha=value[3], defaults={'Nombre': key})
 
-            json_datos.close()
+        json_datos.close()
     
 
 class JuegoConfig(AppConfig):

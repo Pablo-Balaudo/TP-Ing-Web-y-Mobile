846d359aefa5bcb59dbfe86fb84812d5f5508389
Pablo-Balaudo <Balaudo.Pablo@protonmail.com>
2020-10-09 22:49:26 -0300

    Commit for deploy 2

--- Juego/apps.py
+++ Juego/apps.py
@@ -4,39 +4,39 @@ from django.db.utils import OperationalError
 
 # Este metodo es para cargar los colores en la base de 
 # datos, desde el archivo json de la carpeta fixtures
-def cargar_colores():
-
-    from Juego.models import Color
-    import json
-
-    ubicacion_archivo_json = 'Juego/fixtures/Colores.json'
-    with open(ubicacion_archivo_json) as json_datos:
-
-        colores = json.load(json_datos)
-
-        for key, value in colores.items():
-            Color.objects.update_or_create(Nombre=key,
-                                           Red=value[0],
-                                           Green=value[1],
-                                           Blue=value[2],
-                                           Alpha=value[3],
-                                           defaults={'Nombre': key})
-
-        json_datos.close()
+# def cargar_colores():
+#
+#     from Juego.models import Color
+#     import json
+#
+#     ubicacion_archivo_json = 'Juego/fixtures/Colores.json'
+#     with open(ubicacion_archivo_json) as json_datos:
+#
+#         colores = json.load(json_datos)
+#
+#         for key, value in colores.items():
+#             Color.objects.update_or_create(Nombre=key,
+#                                            Red=value[0],
+#                                            Green=value[1],
+#                                            Blue=value[2],
+#                                            Alpha=value[3],
+#                                            defaults={'Nombre': key})
+#
+#         json_datos.close()
     
 
-class JuegoConfig(AppConfig):
-    name = 'Juego'
-
-    # Todo lo que se encuentre en este metodo se ejecutará
-    # al iniciar el sitio, antes de que cualquiera acceda a este
-    def ready(self):
-        
-        try:
-            cargar_colores()
-        except OperationalError:
-            print('Aun no se cargó el modelo de datos')
-            
-        # Este TRY es porque, al ejecutarse esto antes que las 
-        # propias migraciones, tira error cuando no existe una base
-        # de datos, pues se puregunta por una tabla de Colores, que aun no existe
+# class JuegoConfig(AppConfig):
+#     name = 'Juego'
+#
+#     # Todo lo que se encuentre en este metodo se ejecutará
+#     # al iniciar el sitio, antes de que cualquiera acceda a este
+#     def ready(self):
+#
+#         try:
+#             cargar_colores()
+#         except OperationalError:
+#             print('Aun no se cargó el modelo de datos')
+#
+#         # Este TRY es porque, al ejecutarse esto antes que las
+#         # propias migraciones, tira error cuando no existe una base
+#         # de datos, pues se puregunta por una tabla de Colores, que aun no existe
